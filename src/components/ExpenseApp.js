794379a7020fbf1bab9c9261ea8b877a81703d0a
setykarimi <setykarimi80@gmail.com>
2022-08-16 19:51:31 +0430

    add AddTransaction Function

--- src/components/ExpenseApp.js
+++ src/components/ExpenseApp.js
@@ -6,9 +6,16 @@ const ExpenseApp = () => {
     const [expense, setExpense] = useState(0);
     const [income, setIncome] = useState(0);
     const [transactions, setTransactions] = useState([])
+    const AddTransaction = (formValues) => {
+        console.log(formValues);
+        setTransactions([...transactions,{...formValues , id: Date.now()}])
+    }
     return (
         <section>
-            <OverViewComponent expense={expense} income={income}/>
+            <OverViewComponent
+             expense={expense} 
+             income={income}
+              addTransaction={AddTransaction} />
             <TransactionComponent transactions={transactions} />
 
         </section>
