499e068a82033291a8be91b4cd5aca5239777ad8
setykarimi <setykarimi80@gmail.com>
2022-08-16 19:50:37 +0430

    add new state for TransactionForm Values

--- src/components/TransactionForm.js
+++ src/components/TransactionForm.js
@@ -1,14 +1,46 @@
-const TransactionForm = () => {
-    return ( <form>
-        <input type="text" name="desc" />
-        <input type="number" name="amount" />
-        <div>
-            <input type="radio" value="expense" name="expense"></input>
-            <label for="expense">expense</label>
-            <input type="radio" value="income" name="income"></input>
-            <label for="income">income</label>
-        </div>
-    </form> );
+import { useState } from "react";
+
+const TransactionForm = ({ addTransaction }) => {
+    const [formValues, setFormValues] = useState({
+        type: "income",
+        desc: "",
+        amount: ""
+    })
+
+    const Changehandler = (e) => {
+        setFormValues({ ...formValues, [e.target.name]: e.target.value })
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        addTransaction(formValues)
+    }
+
+    return (
+        <form onSubmit={submitHandler}>
+            <input type="text" name="desc" value={formValues.desc} onChange={Changehandler} />
+            <input type="number" name="amount" value={formValues.amount} onChange={Changehandler} />
+
+            <div>
+                <input
+                    type="radio"
+                    value="expense"
+                    name="type"
+                    onChange={Changehandler}
+                    checked={formValues.type === 'expense'}
+                    />
+                <label>expense</label>
+                <input
+                    type="radio"
+                    value="income"
+                    name="type"
+                    onChange={Changehandler}
+                    checked={formValues.type === 'income'}
+                    />
+                <label>income</label>
+            </div>
+            <button type="submit">Add Transaction</button>
+        </form>);
 }
- 
+
 export default TransactionForm;
