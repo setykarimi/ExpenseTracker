6b402246a0cd0d27011226c0161a694cd6572b83
setykarimi <setykarimi80@gmail.com>
2022-08-18 18:06:46 +0430

    Add search function to TransactionComp

--- src/components/TransactionComponent.js
+++ src/components/TransactionComponent.js
@@ -1,12 +1,44 @@
+import { useEffect, useState } from "react";
+
 const TransactionComponent = ({transactions}) => {
-    return ( <section>
-        {transactions.map((t) =>
-         <div key={t.idb}>
-            <p>{t.desc}</p>
-            <p>{t.amount}</p>
-            <p>{t.type}</p>
-        </div>)}
-    </section> );
+    const [searchItem, setSearchItem] = useState("");
+    const [filteredTnx, setFilteredTnx] = useState(transactions);
+
+    const filterTransaction = (search) => {
+        
+        if(search === ""){
+            setFilteredTnx(transactions)
+            return
+        }
+    
+
+        const filteredList =  transactions.filter((t) => t.desc.toLowerCase().includes(search.toLowerCase()));
+        setFilteredTnx(filteredList)
+    }
+
+    const changeHandler = (e) => {
+        setSearchItem(e.target.value)
+        filterTransaction(e.target.value)
+    }
+
+    useEffect(() => {
+        filterTransaction(searchItem)
+    }, [transactions])
+
+
+    return (
+        <section>
+            <input type="text"
+             value={searchItem} 
+             onChange={changeHandler} 
+             />
+            {filteredTnx.length ?  filteredTnx.map((t,index) =>
+                <div key={index}>
+                    <p>{t.desc}</p>
+                    <p>{t.amount}</p>
+                    <p>{t.type}</p>
+                </div>) : "no transaction finds"}
+        </section>);
 }
- 
+
 export default TransactionComponent;
