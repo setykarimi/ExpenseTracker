35a467e68664537193b1725da5ff0838b134215c
setykarimi <setykarimi80@gmail.com>
2022-08-19 01:15:24 +0430

    add icon for addButton, add new classes for styles

--- src/components/OverViewComponents.js
+++ src/components/OverViewComponents.js
@@ -1,25 +1,26 @@
 import { useState } from "react";
 import TransactionForm from "./TransactionForm";
+import { BsPlusLg } from "react-icons/bs";
 
 const OverViewComponent = ({ income, expense, addTransaction }) => {
     const [isShow, setIsShow] = useState(false)
 
     return (
-        <div>
-            <div>
-                <p>Balance: {income - expense}</p>
-                <button onClick={() => setIsShow((prevState) => !prevState)}>
-                    {isShow ? "Cancel" : "Add"}</button>
+        <>
+            <div className="header">
+                <p>Balance: <span className="bold">{income - expense}</span></p>
+                <button onClick={() => setIsShow((prevState) => !prevState)} className={isShow ? "cancel" : "add"}>
+                    {isShow ? "Cancel" : <span>Add <BsPlusLg /></span>}</button>
             </div>
             {isShow && <TransactionForm
              addTransaction={addTransaction}
                 setIsShow={setIsShow}
              />}
-            <div>
-                <div>Expense: {expense}</div>
-                <div>Income: {income}</div>
+            <div className="my-transactions">
+                <div>Expense: <span className="bold red">{expense}$</span></div>
+                <div>Income: <span className="bold green">{income}$</span></div>
             </div>
-        </div>
+        </>
     );
 }
 
