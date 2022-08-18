654983c3793cb04483acf8e8b8187b91a7c50bbe
setykarimi <setykarimi80@gmail.com>
2022-08-19 01:14:44 +0430

    add deleteHandler function, add new classes for styles

--- src/components/ExpenseApp.js
+++ src/components/ExpenseApp.js
@@ -6,25 +6,37 @@ const ExpenseApp = () => {
     const [expense, setExpense] = useState(0);
     const [income, setIncome] = useState(0);
     const [transactions, setTransactions] = useState([])
+    
     const AddTransaction = (formValues) => {
-        console.log(formValues);
-        setTransactions([...transactions,{...formValues , id: Date.now()}])
+        setTransactions([...transactions, { ...formValues, id: Date.now() }])
+    }
+
+    const deleteHandler = (id) => {
+        const filteredTransaction = transactions.filter(t => t.id !== id)
+        setTransactions(filteredTransaction)
     }
 
     useEffect(() => {
         let expense = 0;
         let income = 0;
-        transactions.forEach( t=> t.type === "expense" ? expense = expense + parseFloat(t.amount) : income = income + parseFloat(t.amount))
+        transactions.forEach(t => t.type === "expense" ? expense = expense + parseFloat(t.amount) : income = income + parseFloat(t.amount))
         setExpense(expense)
         setIncome(income)
-    },[transactions])
+    }, [transactions])
+
+
     return (
-        <section>
+        <section className="expense-app">
             <OverViewComponent
-             expense={expense} 
-             income={income}
-              addTransaction={AddTransaction} />
-            <TransactionComponent transactions={transactions} />
+                expense={expense}
+                income={income}
+                addTransaction={AddTransaction}
+            />
+
+            <TransactionComponent
+                transactions={transactions}
+                onDelete={deleteHandler}
+            />
 
         </section>
     );
