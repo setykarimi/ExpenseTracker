3784ca2e6bf22c11b56fe66daca9aca3ac37dca9
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-12-01 19:39:14 +0330

    Add pageTitle

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ExpenseApp from './components/ExpenseApp';
 function App() {
   return (
     <div className="App">
+      <h1 className='page-title'>ExpenseApp</h1>
       <ExpenseApp />
     </div>
   );
